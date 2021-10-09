6c7423ccbedf71016372176508f10d6ae1c7dede
networkgangster <abdussamed_koekdal@hotmail.de>
2021-10-10 01:42:18 +0200

    ajax xmlhttp to random fact api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import './styles/style.scss';
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+// import './styles/style.scss';
 
-const hello = () => console.log('hello world!');
+const XMLHttpRequest = require('xhr2');
 
-hello();
+const btn = document.getElementById('btn');
+
+function chucknorris() {
+  const data = null;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open('GET', 'https://asli-fun-fact-api.herokuapp.com/');
+  xhr.send(data);
+}
+
+btn.addEventListener('click', chucknorris);

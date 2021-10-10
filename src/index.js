33a04443b8e75ee6aef0f1a5a86f40b20c6e0596
networkgangster <abdussamed_koekdal@hotmail.de>
2021-10-10 18:40:19 +0200

    api data into html table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
@@ -7,19 +8,39 @@ const XMLHttpRequest = require('xhr2');
 
 const btn = document.getElementById('btn');
 
+function buildTable(data) {
+  const table = document.getElementById('myTable');
+
+  for (let i = 0; i < data.length - 2; i++) {
+    const row = `<tr>
+                        <td>${data[i]}</td>
+                        <td>${data[i + 1]}</td>
+                        <td>${data[i + 2]}</td>
+                  </tr>`;
+    table.innerHTML += row;
+  }
+}
+
 function chucknorris() {
-  const data = null;
+  const send = null;
+  let data = null;
+  // eslint-disable-next-line prefer-const
+  let myArray = [];
 
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener('readystatechange', function () {
     if (this.readyState === this.DONE) {
-      console.log(this.responseText);
+      data = JSON.parse(this.responseText);
+      myArray.push(data.data.cat, data.data.fact, data.data.hits);
+      buildTable(myArray);
+      console.log(data.data.cat);
+      console.log(myArray);
     }
   });
 
   xhr.open('GET', 'https://asli-fun-fact-api.herokuapp.com/');
-  xhr.send(data);
+  xhr.send(send);
 }
 
 btn.addEventListener('click', chucknorris);
